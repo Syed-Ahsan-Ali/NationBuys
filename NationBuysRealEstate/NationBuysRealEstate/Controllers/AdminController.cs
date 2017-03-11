e9d4a63e3ded7702147184c4bb7ee6d9544f46ca
Naeem408 <bcsf13a533@pucit.edu.pk>
2017-03-11 05:06:28 -0800

    Admin Controller

---
+++ NationBuysRealEstate/NationBuysRealEstate/Controllers/AdminController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace NationBuysRealEstate.Controllers
+{
+    public class AdminController : Controller
+    {
+        //
+        // GET: /Admin/
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: /Admin/
+        public ActionResult Index1()
+        {
+            return View();
+        }
+
+	}
+}
---
+++ NationBuysRealEstate/NationBuysRealEstate/Controllers/AdminMainController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace NationBuysRealEstate.Controllers
+{
+    public class AdminMainController : Controller
+    {
+        //
+        // GET: /AdminMain/
+        public ActionResult Index()
+        {
+            return View();
+        }
+	}
+}

9530fd34faa8e0bc310df98362ddc5bca6f16898
Mudassar-Ali-Riaz <mudassarali199428@gmail.com>
2017-03-24 18:01:16 +0500

    model

---
+++ NationBuysRealEstate/NationBuysRealEstate/Models/Prop_W_Complete_Details.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace NationBuysRealEstate.Models
+{
+    public class Prop_W_Complete_Details
+    {
+       public Property Property { set; get; }
+        public Room Room { set; get; }
+        public Address Address { set; get; }
+    }
+}
---
+++ NationBuysRealEstate/NationBuysRealEstate/Models/PropertyDetail.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace NationBuysRealEstate.Models
+{
+    public class PropertyDetail
+    {
+        public Property Properties { get; set; }
+        public User Users { get; set; }
+        
+         
+    }
+}
